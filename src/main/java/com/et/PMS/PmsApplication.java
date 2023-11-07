9703817761813810e72637407e52b835164150a5
Nardos <nardosm9129@gmail.com>
2023-11-07 14:48:31 +0300

    Update controller

--- src/main/java/com/et/PMS/PmsApplication.java
+++ src/main/java/com/et/PMS/PmsApplication.java
@@ -1,11 +1,11 @@
 package com.et.PMS;
 
+import org.hibernate.cfg.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PmsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PmsApplication.class, args);
 	}
--- src/main/java/com/et/PMS/controller/PatientController.java
+++ src/main/java/com/et/PMS/controller/PatientController.java
@@ -3,6 +3,7 @@ package com.et.PMS.controller;
 
 import com.et.PMS.dto.PatientRequestDto;
 import com.et.PMS.dto.ResponseDTO;
+import com.et.PMS.entities.Patient;
 import com.et.PMS.service.PatientService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,17 +14,17 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/")
 public class PatientController {
     private final PatientService patientService;
     @Autowired
     private static final String JSON = MediaType.APPLICATION_JSON_VALUE;
 
-    @PostMapping(value = "patient/register",consumes = JSON,produces = JSON)
+    @PostMapping(value = "/patient/register")
     public ResponseDTO<?> registerPatient(@RequestBody PatientRequestDto patientRequestDto ){
         return patientService.registerPatient(patientRequestDto);
     }
-    @GetMapping(value = "get/patients", consumes = JSON)
+    @GetMapping(value = "/patients")
     public ResponseDTO<?> getPatients(){
         return patientService.getPatients();
     }
